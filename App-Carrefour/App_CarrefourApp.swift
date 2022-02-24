82bc85ea7bd7a290a6c69b42c26541222d0084d3
Bryan Caro <bryancaro@icloud.com>
2022-02-24 10:32:26 +0100

    Initial Commit

---
+++ App-Carrefour/App_CarrefourApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_CarrefourApp.swift
+//  App-Carrefour
+//
+//  Created by Bryan Caro on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct App_CarrefourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-CarrefourUITests/App_CarrefourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  App_CarrefourUITestsLaunchTests.swift
+//  App-CarrefourUITests
+//
+//  Created by Bryan Caro on 2/24/22.
+//
+
+import XCTest
+
+class App_CarrefourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
